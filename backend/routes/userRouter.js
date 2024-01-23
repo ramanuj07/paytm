f0eab393a7db49880134704eb7cb4274b74afee9
Ramanuj <ramanuj0705@gmail.com>
2024-01-24 02:39:59 +0530

    Backend done

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const zod = require("zod");
-const User = require("../db");
-const jwt = require("jsonwebtoken");
-const JWT_SECRET_KEY = require("../config");
 const router = express.Router();
+const zod = require("zod");
+const { User, Account } = require("../db");
+const jwt = require("jsonwebtoken");
+const { authMiddleware } = require("../middleware");
+const { JWT_SECRET_KEY } = require("../config");
 
 const signupSchema = zod.object({
   username: zod.string().email(),
@@ -17,32 +18,41 @@ const signinSchema = zod.object({
   password: zod.string(),
 });
 
+// SIGN-UP ROUTE
 router.post("/signup", async (req, res) => {
-  const body = req.body;
-
-  const { success } = signupSchema.safeParse(body);
-
+  const { success } = signupSchema.safeParse(req.body);
   if (!success) {
-    return res.json({
-      message: "Email already taken / Incorrect inputs",
-    });
-  }
-
-  const existingUser = await User.findOne({
-    username: body.username,
-  });
-
-  if (existingUser) {
     return res.status(411).json({
       message: "Email already taken / Incorrect inputs",
     });
   }
 
-  const user = await User.create(body);
+  const existingUser = await User.findOne({
+    username: req.body.username,
+  });
+
+  if (existingUser) {
+    return res.status(411).json({
+      message: "Email already taken/Incorrect inputs",
+    });
+  }
+
+  const user = await User.create({
+    username: req.body.username,
+    password: req.body.password,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+  });
+  const userId = user._id;
+
+  await Account.create({
+    userId,
+    balance: 1 + Math.random() * 10000,
+  });
 
   const token = jwt.sign(
     {
-      userId: user._id,
+      userId,
     },
     JWT_SECRET_KEY
   );
@@ -53,6 +63,7 @@ router.post("/signup", async (req, res) => {
   });
 });
 
+// SIGN-IN ROUTE
 router.post("/signin", async (req, res) => {
   const body = req.body;
 
@@ -68,8 +79,78 @@ router.post("/signin", async (req, res) => {
     username: body.username,
     password: body.password,
   });
+
+  if (user) {
+    const token = jwt.sign(
+      {
+        userId: user._id,
+      },
+      JWT_SECRET_KEY
+    );
+
+    res.json({
+      token: token,
+    });
+    return;
+  }
+
+  res.status(411).json({
+    message: "Error while logging in",
+  });
+
+  // UPDATE CREDENTIALS
+  const updateBody = zod.object({
+    password: zod.string().optional(),
+    firstName: zod.string().optional(),
+    lastName: zod.string().optional(),
+  });
+
+  router.put("/", authMiddleware, async (req, res) => {
+    const success = updateBody.safeParse(req.body);
+
+    if (!success) {
+      res.status(411).json({
+        message: "Error while updating information",
+      });
+    }
+
+    await user.updateOne(req.body, {
+      id: req.userId,
+    });
+
+    res.json({
+      message: "Updated successfully",
+    });
+  });
+
+  // GET USERS IN BULK
+  router.get("/bulk", async (req, res) => {
+    const filter = req.query.filter || "";
+
+    const users = await User.find({
+      $or: [
+        {
+          firstName: {
+            $regex: filter,
+          },
+        },
+        {
+          lastName: {
+            $regex: filter,
+          },
+        },
+      ],
+    });
+
+    res.json({
+      user: users.map((user) => ({
+        username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        _id: user._id,
+      })),
+    });
+  });
 });
 
-module.exports = {
-  router,
-};
+module.exports = router;
